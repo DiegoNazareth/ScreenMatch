53288591bf4fed94249f3c4cc3750c831dc6a402
DiegoNazareth <toby72korn@hotmail.com>
2024-07-01 01:37:33 -0600

    Controlando el acceso a la escritura de datos

--- Principal.java
+++ Principal.java
@@ -1,9 +1,12 @@
+import com.aluracursos.screenmatch.modelos.Pelicula;
+
 public class Principal {
     public static void main(String[] args) {
         Pelicula miPelicula = new Pelicula();
-        miPelicula.nombre="TOy";
-        miPelicula.fechaDeLanzamiento=2000;
-        miPelicula.duracionEnMinutos=102;
+        miPelicula.setNombre("TOy");
+        miPelicula.setFechaDeLanzamiento(2000);
+        miPelicula.setDuracionEnMinutos(102);
+        miPelicula.setIncluidoEnElPlan(true);
 
         miPelicula.muestraFichaTecnica();
 
@@ -11,15 +14,15 @@ public class Principal {
         miPelicula.evalua(9.4);
         miPelicula.evalua(8);
 
-        System.out.println(miPelicula.sumaDeLasEvaluaciones);
-        System.out.println((miPelicula.totalDeLasEvaluaciones));
+
+        System.out.println((miPelicula.getTotalDeLasEvaluaciones()));
         System.out.println(miPelicula.calculaMedia());
 
-        otraPelicula.nombre="CARS";
-        otraPelicula.fechaDeLanzamiento=2004;
-        otraPelicula.duracionEnMinutos=120;
-
-        //otraPelicula.muestraFichaTecnica();
+//        otraPelicula.nombre="CARS";
+//        otraPelicula.fechaDeLanzamiento=2004;
+//        otraPelicula.duracionEnMinutos=120;
+//
+//        //otraPelicula.muestraFichaTecnica();
 
     }
 }
